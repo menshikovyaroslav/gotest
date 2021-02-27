d7f34ba3af3de8c683891a5c604c2e30422f1a9f
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2021-02-27 17:35:08 +0700

    completed read.go

--- read/read.go
+++ read/read.go
@@ -1,26 +1,61 @@
 package main
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
 )
+
+type P struct {
+	fname string
+	lname string
+}
 
 func main() {
 
-	person := make(map[string]string)
-	var name string
-	var address string
+	sli := []P{}
 
-	fmt.Printf("Name: ")
+	var name string
+	fmt.Printf("Enter the name of a text file: ")
 	fmt.Scan(&name)
 
-	fmt.Printf("Address: ")
-	fmt.Scan(&address)
+	file, err := os.Open(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-	person["name"] = name
-	person["address"] = address
-	barr, _ := json.Marshal(person)
-	fmt.Println(string(barr))
+	//data := make([]byte, 64)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+
+		if err != nil && err != io.EOF {
+			break
+		}
+
+		var line = scanner.Text()
+		words := strings.Split(line, " ")
+		if len(words) != 2 {
+			continue
+		}
+
+		fName := words[0]
+		lName := words[1]
+
+		var p = P{fname: fName, lname: lName}
+		sli = append(sli, p)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for index, value := range sli {
+		fmt.Printf("next slice item: %v %s %s \n", index, value.fname, value.lname)
+	}
 
 	var txt string
 	fmt.Scan(&txt)
